48d3576502eba278d0b33486ff7dd02f9904462c
Arexan-K <94745953+ArexanK@users.noreply.github.com>
2023-12-18 14:51:20 +0100

    static site generation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
-    import '../lib/styles/fonts.css';
-    import { Header, Footer } from "../lib/index.js";
+	import '../lib/styles/fonts.css';
+	import { Header, Footer } from '../lib/index.js';
+
+	export const prerender = true;
 </script>
 
 <Header />
@@ -10,52 +12,48 @@
 <Footer />
 
 <style>
+	:global(*) {
+		box-sizing: border-box;
+		text-decoration: none;
+		list-style: none;
+		margin: 0;
+		padding: 0;
+		font-family: var(--Apercu-font);
+	}
 
-    :global(*){
-        box-sizing: border-box;
-        text-decoration: none;
-        list-style: none;
-        margin: 0;
-        padding: 0;
-        font-family: var(--Apercu-font);
-    }
+	/* Color variable */
 
-    /* Color variable */
+	:global(:root) {
+		--Bg-Dark-Blue: #051d1d;
 
-    :global(:root){
+		--Yellow-Primary: #ffda1d;
+		--Blue-Secondary: #3dbae1;
 
-        --Bg-Dark-Blue: #051d1d;
+		--White: #ffffff;
+		--Black: #000;
 
-        --Yellow-Primary: #FFDA1D;
-        --Blue-Secondary: #3DBAE1;
+		--Apercu-Font: 'Apercu pro', sans-serif;
+		--Paragraph-Font: 'Jubilat', serif;
+		--Header-Font: 'Kirang Haerang', sans-serif;
+	}
 
-        --White: #FFFFFF;
-        --Black: #000;
+	:global(body) {
+		background-color: var(--Bg-Dark-Blue);
+		color: var(--White);
+		width: 100%;
+		height: 100vh;
+		overflow: hidden;
+		overflow-y: scroll;
+	}
 
-        --Apercu-Font: 'Apercu pro', sans-serif;
-        --Paragraph-Font: 'Jubilat', serif;
-        --Header-Font: 'Kirang Haerang', sans-serif;
+	:global(h1) {
+		font-family: var(--Header-Font);
+		font-size: 4em;
+		padding: 0.5em;
+		color: var(--Yellow-Primary);
+	}
 
-    }
-
-    :global(body) {
-        background-color: var(--Bg-Dark-Blue);
-        color: var(--White);
-        width: 100%;
-        height: 100vh;
-        overflow: hidden;
-        overflow-y: scroll;
-    }
-
-    :global(h1){
-        font-family: var(--Header-Font);
-        font-size: 4em;
-        padding: .5em;
-        color: var(--Yellow-Primary);
-    }
-
-    :global(h2){
-        font-family: var( --Paragraph-Font);
-    }
-
+	:global(h2) {
+		font-family: var(--Paragraph-Font);
+	}
 </style>
